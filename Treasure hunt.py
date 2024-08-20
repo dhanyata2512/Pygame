c0f54c07a671bd46d56eeca6c77c7f9c6a6a78b6
D.P <dhanyata2512@gmail.com>
2024-08-20 18:57:51 +0100

    Update Treasure hunt.py

--- Treasure hunt.py
+++ Treasure hunt.py
@@ -12,18 +12,35 @@ class Pirate (pygame.sprite.Sprite):
     def __init__ (self):
         super().__init__()
         self.image=pygame.image.load("pirate.png")
+        self.image=pygame.transform.scale(self.image,(50,100))
         self.rect=self.image.get_rect()
+
+class Stone (pygame.sprite.Sprite):
+
+    def __init__(self,gem):
+        super().__init__()
+        self.image=pygame.image.load(gem)
+        self.rect=self.image.get_rect() 
+        self.rect.x=random.randint(0,w)
+        self.rect.y=random.randint(0,h)      
 
 pirate=Pirate()
 pirate_group=pygame.sprite.Group()
 pirate_group.add(pirate)
 
 
+gem_pic=["bgem.png","cgem.png","rgem.png"]
+gem_group=pygame.sprite.Group()
+
+for i in range(100):
+    stone=Stone(random.choice(gem_pic))
+    gem_group.add(stone)
 
 run=True
 while run:
     screen.blit(bg,(0,0))
     pirate_group.draw(screen)
+    gem_group.draw(screen)
     pygame.display.update()
 
     for event in pygame.event.get():
